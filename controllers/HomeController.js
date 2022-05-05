6d8dafec78af2c2fee696116abef027606620222
MetaYean <meta.y@tecmovecorp.com>
2022-05-05 17:59:52 +0700

    first

---
+++ controllers/HomeController.js
@@ -0,0 +1,25 @@
+
+const { exec } = require('node:child_process')
+
+exports.index = async (req, res, next) => {
+    try {
+
+        exec('ls', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                console.log('1======')
+            return;
+            }
+                // console.log(`stdout: ${stdout}`);
+                // console.log('2======')
+                // console.error(`stderr: ${stderr}`);
+                let data = JSON.stringify(stdout);
+                console.log(data)
+
+        });
+
+        res.render('index', { title: 'Express' });
+    } catch (error) {
+        console.log(error);
+    }
+}
---
+++ routes/route.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+var HomeController = require('./../controllers/HomeController');
+
+/* GET home page. */
+router.get('/', HomeController.index);
+
+
+module.exports = router;
